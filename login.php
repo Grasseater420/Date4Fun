ea461150ed72016dba57291b371438e54e6da556
Neireau <k.t.c.gravenhorst@me.com>
2017-10-31 14:13:06 +0100

    Barebones
    Enkel een form tot nog toe.

--- login.php
+++ login.php
@@ -1,8 +1,9 @@
-<html>
-    <head>
-        <title>Login Page Date4Fun</title>
-    </head>
-    <body>
-        Login blablabla.
-    </body>
-</html>
+<form action="" method="post">
+    <label><b>Gebruikersnaam</b></label><br>
+    <input type="text" placeholder="Gebruikersnaam" name="username" required><br>
+
+    <label><b>Wachtwoord</b></label><br>
+    <input type="password" placeholder="Wachtwoord" name="password" required><br>
+
+    <button type="submit">Login</button>
+</form>
