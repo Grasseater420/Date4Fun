bc155f8f97175fb7a3aa1dcf4bd188d2f35807a0
Neireau <k.t.c.gravenhorst@me.com>
2017-11-01 12:52:09 +0100

    Cleaning
    Syntax aangepast

--- login.php
+++ login.php
@@ -1,47 +1,44 @@
 <?php
+
   session_start();
-  // sessie beginnen
-  include ('config.php');
+  include('config.php');
 
-  if (!empty($_POST)){
-  // controleren of pagina correct is aangeroepen.
-    $gebruiker = mysqli_real_escape_string($db, $_POST['gebruikersnaam']);
+  if (!empty($_POST)) {
+    $gebruiker  = mysqli_real_escape_string($db, $_POST['gebruikersnaam']);
     $wachtwoord = mysqli_real_escape_string($db, $_POST['wachtwoord']);
-    $query = "SELECT * FROM gebruikers WHERE gebruikersnaam ='" . $_POST["gebruikersnaam"] ."'AND wachtwoord='" . $_POST["wachtwoord"] ."'";
-    $result = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
+    $query      = "SELECT * FROM gebruikers WHERE gebruikersnaam ='" . $_POST["gebruikersnaam"] ."'AND wachtwoord='" . $_POST["wachtwoord"] ."'";
+    $result     = mysqli_query($db, $query) or die("FOUT : " . mysqli_error());
 
-    if (mysqli_num_rows($result) > 0){
-  // gebruikersnaam gevonden, registreer gegevens in session
-      $_SESSION["auth"]=true;
-  //auth controleert of een klant is ingelogd
-      $_SESSION["timeout"]=time() + 120;
-			$_SESSION["gebruiker"]=$gebruiker;
-		  while($row = mysqli_fetch_assoc($result)) {
-        $rol = $row['admin'];
+    if (mysqli_num_rows($result) > 0) {
+      $_SESSION["auth"]      = true;
+      $_SESSION["timeout"]   = time() + 120;
+			$_SESSION["gebruiker"] = $gebruiker;
+
+      while ($row = mysqli_fetch_assoc($result)) {
+        $isAdmin = $row['admin'];
       }
-  // Doorsturen naar beveiligde pagina
-      if(($rol) == "1") {
-        // header("Location: admin.php");
+
+      if ($isAdmin == true) {
         echo "Je bent Admin";
         exit();
       }
-      elseif(($rol =="0")) {
-        // header("Location: medewerker.php");
+      elseif ($isAdmin ==false) {
         echo "Je bent Gebruiker";
         exit();
       }
+
     }
-    else{
-	// geen e-mail adres gevonden, of ongeldig wachtwoord.
+    else {
       $tekst = "U hebt geen geldige combinatie van e-mailadres en wachtwoord opgegeven.<br>
       Maak een keuze: <br>
       <a href=\"loginForm.php\">Opnieuw inloggen</a><br>
       <a href=\"registreerForm.php\">Hier registreren</a><br>";
       die($tekst);
     }
+
   }
-  else{
-	// pagina was incorrect aangeroepen, direct doorsturen naar login.php
-    header("Location:http://poki.nl/my-little-pony ");
+  else {
+    header("Location:loginForm.php");
   }
+
 ?>
