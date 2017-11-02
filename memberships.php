925588cd52f0afac2d52987be6c799c2986c6955
Grasseater420 <33255099+Grasseater420@users.noreply.github.com>
2017-11-02 17:21:20 +0100

    Add files via upload

--- memberships.php
+++ memberships.php
@@ -8,25 +8,31 @@
   <h1>Logo</h1>
   <h2>Welkom bij Date4Fun Memberships!</h2>
 
-  <div class="membership">
-  <h3>Goud</h3>
-  <p><?php include "lorem.php"; echo $lorem; ?></p>
-  <h2>€7,50</h2>
-  <button type="button">In Winkelwagen</button>
-  </div>
+  <?php
+  include "connection.php";
+  function laatMemZien(){
+    //Connectie met de database
+    //Functie om membership_prijs te verkrijgen MOET NOG KOMEN
+    include "connection.php";
 
-  <div class="membership">
-  <h3>Zilver</h3>
-  <p><?php include "lorem.php"; echo $lorem; ?></p>
-  <h2>€10,-</h2>
-  <button type="button">In Winkelwagen</button>
-  </div>
+    //De Query om titel en omschrijving te verkijgen
+    $query = "SELECT membership_id, titel, omschrijving FROM `membership`";
+    $result = mysqli_query($db, $query);
 
-  <div class="membership">
-  <h3>Brons</h3>
-  <p><?php include "lorem.php"; echo $lorem; ?></p>
-  <h2>€12,50</h2>
-  <button type="button">In Winkelwagen</button>
-  </div>
+    //Resultaten naar het scherm
+    while($row = mysqli_fetch_assoc($result)){
+        echo "
+        <div class='membership'>
+          <h3>".$row['titel']."</h3>
+          <p>".$row['omschrijving']."</p>
+          <h2>Prijs</h2>
+          <button type='button'>In Winkelwagen</button>
+        </div>
+      ";
+    }
+  }
+  laatMemZien();
+  mysqli_close($db)
+   ?>
 </body>
 </html>
