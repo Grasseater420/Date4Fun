b5ad190670cb99072243bf585e0194f89ec41e13
Grasseater420 <33255099+Grasseater420@users.noreply.github.com>
2017-11-02 14:30:28 +0100

    Add files via upload

---
+++ memberships.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>Date4Fun</title>
+</head>
+<body>
+  <h1>Date4Fun</h1>
+  <h1>Logo</h1>
+  <h2>Welkom bij Date4Fun Memberships!</h2>
+
+  <div class="membership">
+  <h3>Goud</h3>
+  <p><?php include "lorem.php"; echo $lorem; ?></p>
+  <h2>€7,50</h2>
+  <button type="button">In Winkelwagen</button>
+  </div>
+
+  <div class="membership">
+  <h3>Zilver</h3>
+  <p><?php include "lorem.php"; echo $lorem; ?></p>
+  <h2>€10,-</h2>
+  <button type="button">In Winkelwagen</button>
+  </div>
+
+  <div class="membership">
+  <h3>Brons</h3>
+  <p><?php include "lorem.php"; echo $lorem; ?></p>
+  <h2>€12,50</h2>
+  <button type="button">In Winkelwagen</button>
+  </div>
+</body>
+</html>
