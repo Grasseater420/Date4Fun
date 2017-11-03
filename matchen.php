0813767400853ff6c0ee4b39905df5ae666f8a8c
Grasseater420 <33255099+Grasseater420@users.noreply.github.com>
2017-11-03 14:28:37 +0100

    Add files via upload

--- matchen.php
+++ matchen.php
@@ -8,12 +8,10 @@
   <h2>Welkom bij de Date4Fun Matchpagina!</h2>
 
 <?php
-  function laatProfielZien(){
+  include "config.php";
+  function laatProfielZien($id){
     //Connectie met Database
-    include "connection.php";
-
-    //Random getal om profiel op te roepen
-    $id = RAND(1,2);
+    include "config.php";
 
     //De query om profielen te verkrijgen
     $query = "SELECT gebruikers.voornaam, profielen.foto, profielen.overmij FROM `gebruikers` INNER JOIN profielen ON gebruikers.gebruiker_id=profielen.gebruikers_id WHERE gebruikers.gebruiker_id = '".$id."'";
@@ -30,12 +28,24 @@
       ";
     }
   }
-  laatProfielZien();
+
+  //Aantal profielen die bestaan
+  $query = "SELECT COUNT(gebruiker_id) FROM gebruikers";
+  $result = mysqli_query($db, $query);
+
+  $row = mysqli_fetch_array($result);
+
+  $total = $row[0];
+
+  //Random getal om profiel op te roepen
+  $id = RAND(1,$total);
+  laatProfielZien($id);
  ?>
 
  <form action="http://localhost/PHP1/Date4Fun%20Project%20files/matchen.php">
      <input type="submit" value="Dislike"/>
  </form>
- <!--Profiel button moet nog komen-->
+
+ <a href="profiel.php?id=<?php echo $id; ?>">Profiel bezoeken</a>
 </body>
 </html>
