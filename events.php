9613368018d9cbe5d9364e0fb021a587738995a1
Neireau <k.t.c.gravenhorst@me.com>
2017-11-06 17:29:11 +0100

    Winkelwagen knoppen
    Links naar winkelwagen geimplementeerd voor Events en Memberships. Memberships ook gestyled.

--- events.php
+++ events.php
@@ -57,63 +57,66 @@
   // ░░░░░▀▄░▄▀█░░░░░█░░▌░░░░▀▄░░░░░█
   ////
 
+  include "config.php";
+
   function renderEvents() {
+
     include "config.php";
-    $query    = "SELECT * FROM events INNER JOIN producten ON producten.event_id=events.event_id";
-    $result   = mysqli_query($db, $query);
+
+    $query  = "
+      SELECT *
+      FROM events
+      INNER JOIN producten
+      ON producten.event_id=events.event_id
+      ";
+    $result = mysqli_query($db, $query);
 
     if (!$result) {
       die("<br> Database query mislukt.");
     }
 
-    ////
-    // Genereer Bootstrap
-    //
     echo "<div class=\"container\"><div class='row'>";
-    //
-    // Maakt container aan waar beide events in getoond worden
-    //
+
     while ($row = mysqli_fetch_assoc($result)) {
-      //
-      // De som van alle kolommen moet twaalf zijn, omdat het twee events
-      // zijn gebruiken we 'col-sm-6' (12 / 2 = 6)
-      //
-      echo "<div class='col-sm-6'>
-              <div class=\"container-fluid\">
-                <div class=\"panel panel-default\">
-                  <div class=\"panel-heading\">
-                    <h3>" . $row['titel'] . "</h3>
-                  </div>
-                  <div class=\"panel-body\" style=\"min-height: 100;\">
-                    <p>" . $row['omschrijving'] . "</p>
-                  </div>
-                  <div class=\"panel-body\" style=\"max-height: 40;\">
-                    <h4 class=\"text-center\">€ " . $row['prijs'] . "</h4>
-                  </div>
-                  <div class=\"panel-body\" style=\"min-height: 50;\">
-                    <div class=\"text-center\">
-                      <button type=\"button\">Winkelmandje</button><br>
-                    </div>
-                  </div>
-                  <div class=\"container-fluid\">
-                    <div class=\"thumbnail\">
-                      <a href=\"" . $row['foto'] . "\">
-                        <img src=\"" . $row['foto'] . "\" class=\"img-responsive\">
-                        <div class=\"caption\">
-                          <h5>" . $row['locatie'] . "</h5>
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                </div>
+      echo "
+      <div class='col-sm-6'>
+        <div class=\"container-fluid\">
+          <div class=\"panel panel-default\">
+            <div class=\"panel-heading\">
+              <h3>" . $row['titel'] . "</h3>
+            </div>
+            <div class=\"panel-body\" style=\"min-height: 100;\">
+              <p>" . $row['omschrijving'] . "</p>
+            </div>
+            <div class=\"panel-body\" style=\"max-height: 40;\">
+              <h4 class=\"text-center\">€ " . $row['prijs'] . "</h4>
+            </div>
+            <div class=\"panel-body\" style=\"min-height: 50;\">
+              <div class=\"text-center\">
+                <a class=\"btn btn-default\" href=\"./winkelwagen.php?add=" . $row['product_id'] . "\">Winkelmandje</a>
               </div>
-            </div>";
+            </div>
+            <div class=\"container-fluid\">
+              <div class=\"thumbnail\">
+                <a href=\"" . $row['foto'] . "\">
+                  <img src=\"" . $row['foto'] . "\" class=\"img-responsive\">
+                  <div class=\"caption\">
+                    <h5>" . $row['locatie'] . "</h5>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      ";
     }
     echo "</div></div>";
   }
 
-?>
+  mysqli_close($db);
 
+?>
 <html>
   <head>
     <?php
@@ -121,7 +124,6 @@
       include "header.php";
 
       renderHead('Date4Fun Events');
-
       renderNavbar();
       // renderJumbotron();
 
