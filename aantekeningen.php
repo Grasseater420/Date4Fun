18945cb2b0b5375161b61c6bbdcb02993d4c2165
Neireau <k.t.c.gravenhorst@me.com>
2017-11-06 11:47:02 +0100

    Push
    Niet omdat het moet, maar omdat het kan.

--- aantekeningen.php
+++ aantekeningen.php
@@ -6,3 +6,8 @@
   $script =  "<script> $(document).ready(function(){ $('#loginModal').modal('show'); }); </script>";
 
  ?>
+
+ SELECT producten.product_id, IFNULL(events.titel, membership.omschrijving), producten.prijs
+ FROM producten
+ 	LEFT JOIN events ON events.event_id=producten.event_id
+     LEFT JOIN membership ON membership.membership_id=producten.membership_id
