07ee40b96bdaad11631dcdf42560171bc2d23ab1
jparengkuan <jim.parengkuan@gmail.com>
2017-11-06 11:48:14 +0100

    Merge origin/master
    Conflicts:
    	login.php

---
+++ winkelwagen_leeg.php
@@ -0,0 +1,75 @@
+<html>
+  <head>
+    <!--<link rel="stylesheet" type="text/css" href="stylesheet.css">-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Date4Fun Events</title>
+    <?php
+
+      include "header.php";
+
+      renderNavbar();
+
+    ?>
+  </head>
+  <body>
+
+
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-xs-8">
+			<div class="panel panel-info">
+				<div class="panel-heading">
+					<div class="panel-title">
+						<div class="row">
+							<div class="col-xs-6">
+								<h5><span class="glyphicon glyphicon-shopping-cart"></span> Winkelwagen</h5>
+							</div>
+							<div class="col-xs-6">
+
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="panel-body">
+					<div class="row">
+						
+						<div class="text-center">
+						<h4>U heeft geen producten in de winkelwagen</h4>
+						</div>
+						
+					</div>
+
+					<div class="row">
+						<div class="text-center">
+							<div class="col-xs-9">
+							</div>
+							<div class="col-xs-3">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="panel-footer">
+					<div class="row text-center">
+						<div class="col-xs-9">
+						</div>
+						<div class="col-xs-3">
+														<button type="button" class="btn btn-primary btn-sm btn-block">
+									<span class="glyphicon glyphicon-share-alt"></span> Verder winkelen
+								</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+</body>
+
+</html>
