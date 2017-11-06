f8ee4ca346114188ad930af60800d0bbc0bb08c2
actionhenkie <33255102+actionhenkie@users.noreply.github.com>
2017-11-06 15:52:09 +0100

    Update matchen.php

--- matchen.php
+++ matchen.php
@@ -1,10 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
 <body>
-  <h1>Date4Fun</h1>
-  <h1>Logo</h1>
-  <h2>Welkom bij de Date4Fun Matchpagina!</h2>
+
 
 <?php
+  include "header.php";
+  renderNavbar();?>
+  <?php
   include "config.php";
+  
   function laatProfielZien($id){
     //Connectie met Database
     include "config.php";
@@ -17,7 +29,7 @@
     while($row = mysqli_fetch_assoc($result)){
       echo "
       <div class='profiel'>
-        <img src='./profielpics".$row['foto']."' height='100' width='100'>
+        <img src='./profielpics".$row['foto']."' height='200' width='200'>
         <h3>".$row['voornaam']."</h3>
         <p>".$row['overmij']."</p>
 		
@@ -27,7 +39,7 @@
   }
 
 
-  //maak een array met alle gebruikers_id's
+  //Aantal profielen die bestaan
   $query = "SELECT gebruikers_id FROM profielen";
   $result = mysqli_query($db, $query);
 
@@ -41,20 +53,25 @@
 	
 
 
-  //kiest 2 willekeurige id's uit de array en laat die zien
+  //Random getal om profiel op te roepen
   $rand_id = array_rand ($nummers, 2);
   $id = $nummers[$rand_id[0]];
   $id2 = $nummers[$rand_id[1]];
 
   laatProfielZien($id);
-  laatProfielZien($id2);
+ 
+ ?>
+<a href="profiel.php?id=<?php echo $id; ?>">Profiel bezoeken</a>
+
+<?php 
+	laatProfielZien($id2);
  ?>
 
+ <a href="profiel.php?id=<?php echo $id2; ?>">Profiel bekijken</a>
+ 
  <form action="http://localhost/Date4Fun/matchen.php">
      <input type="submit" value="Nieuwe vrijgezelen bekijken"/>
  </form>
 
- <a href="profiel.php?id=<?php echo $id; ?>">Profiel1 bezoeken</a>
- <a href="profiel.php?id=<?php echo $id2; ?>">Profiel2 bezoeken</a>
 </body>
 </html>
