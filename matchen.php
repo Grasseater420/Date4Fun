910928894de78e544f97fbbe0d9b984cc98f8392
Neireau <k.t.c.gravenhorst@me.com>
2017-11-09 13:55:02 +0100

    Push Matchen
    Matchen is netjes en van comments voorzien door de auteur.

--- matchen.php
+++ matchen.php
@@ -1,54 +1,137 @@
 <?php
 
   session_start();
+
+  ////// Neireau:
+  //
+  // Het starten van een sessie genereerd een notice, geen foutmelding dus,
+  // maar die willen we natuurlijk niet tonen vandaar onderstaande regel.
+  //
+  //////
+
   error_reporting(0);
 
-          if ($_SESSION['gebruikers_id'] == NULL)
-        {
+  ////// Neireau:
+  //
+  // Als een bezoeker niet is ingelogd werkt de Events pagina niet, dit komt
+  // doordat het script de gebruiker identifiseerd vanuit de sessie, om dit te
+  // voorkomen is besloten dat de pagina niet zichtbaar is wanneer je niet bent
+  // ingelogd, ofwel wanneer er geen sessie is.
+  //
+  // In het initiële plan haddden memberships nog geen functie, gaandeweg het
+  // project is besloten dat de site zonder membershit niet gebruikt kan worden.
+  // Het tweede script controleerd dus of de membership-status 'Gratis' is, is
+  // dit het geval wordt je verwezen naar de pagina Memberships.
+  //
+  //////
 
-             header("Location:index.php");
-        }
-               if ($_SESSION['membership'] == "Gratis")
-{
+  if ($_SESSION['gebruikers_id'] == NULL) {
+     header("Location:index.php");
+  }
+
+  if ($_SESSION['membership'] == "Gratis") {
     header("Location: memberships.php");
-}
+  }
 
-
+  ////// Neireau:
+  //
+  // De Events pagina maakt gebruik van de functie 'showProfielFoto' welke in
+  // 'getImage.php' is uitgewerkt. De functie haalt de afbeeldingen op uit de
+  // database en maakt gebruik van de standaard functie base64_encode().
+  // In 'config.php' staan de database credentials opgeslagen zodat deze niet
+  // constant overgenomen hoeven te worden.
+  //
+  //////
 
   include "config.php";
   include "getImage.php";
-  // var_dump($_SERVER['PHP_SELF']);
+
+  ////// Neireau:
+  //
+  // Onderstaande functie wordt in de body aangeroepen om de content te genereren,
+  // ik heb de functie van comments voorzien zodat dit voor ieder leesbaar zou
+  // moeten zijn.
+  //
+  //////
+
   function renderMatch() {
 
     include 'config.php';
 
-    // Eerst moeten we onze eigen interesse weten.
-    $query  = "
-      SELECT profielen.geintereseerd, gebruikers.geslacht
+    ////// Neireau:
+    //
+    // Eerste poging om te matchen op basis van geslacht en interesse.
+    // Deze code is inmiddels obsolete omdat ik alles in één query heb weten te
+    // krijgen. Ik heb de code laten staan zodat mijn eigen progressie zichtbaar
+    // is.
+    //
+    //////
+    //
+    // $query  = "
+    //   SELECT profielen.geintereseerd, gebruikers.geslacht
+    //   FROM gebruikers
+    //   INNER JOIN profielen
+    //   ON gebruikers.gebruiker_id=profielen.gebruikers_id
+    //   WHERE gebruikers.gebruiker_id = '" . $_SESSION['gebruikers_id'] . "'
+    //   ";
+    // $result = mysqli_query($db, $query) or die("FOUT: " . mysqli_error());
+    //
+    // while ($row = mysqli_fetch_assoc($result)) {
+    //   $geintereseerd  = $row['geintereseerd'];
+    //   $geslacht       = $row['geslacht'];
+    // }
+    //
+    // $query2  = "
+    //   SELECT profielen.gebruikers_id, gebruikers.voornaam, profielen.foto, profielen.overmij
+    //   FROM `gebruikers`
+    //   INNER JOIN profielen
+    //   ON gebruikers.gebruiker_id=profielen.gebruikers_id
+    //   WHERE gebruikers.geslacht='" . $geintereseerd . "'
+    //   AND profielen.geintereseerd='" . $geslacht . "'
+    //   ";
+    // $result2 = mysqli_query($db, $query2) or die("FOUT: " . mysqli_error());
+    //
+    //////
+
+    ////// Neireau:
+    //
+    // Deze query bestaat uit drie componenten; ik moet mijn eigen geslacht uit
+    // de tabel gebruikers weten, vervolgens mijn interesse uit de tabel
+    // profielen. Wanneer ik deze beide gegevens heb in de sub-query kan ik
+    // door gebruikers en profielen te joinen alleen de kandidaten opvragen
+    // die geintereseerd zijn in mijn geslacht en waarvan het geslacht overeen
+    // komt met mijn interesse.
+    //
+    //////
+
+    $query = "
+      SELECT profielen.gebruikers_id, gebruikers.voornaam, profielen.foto, profielen.overmij
       FROM gebruikers
       INNER JOIN profielen
-      ON gebruikers.gebruiker_id=profielen.gebruikers_id
-      WHERE gebruikers.gebruiker_id = '" . $_SESSION['gebruikers_id'] . "'
+        ON gebruikers.gebruiker_id=profielen.gebruikers_id
+        WHERE profielen.geintereseerd IN
+          (SELECT gebruikers.geslacht
+          FROM gebruikers
+            WHERE gebruikers.gebruiker_id='" . $_SESSION['gebruikers_id'] . "')
+        AND gebruikers.geslacht IN
+          (SELECT profielen.geintereseerd
+          FROM gebruikers
+          LEFT JOIN profielen
+            ON gebruikers.gebruiker_id=profielen.gebruikers_id)
       ";
     $result = mysqli_query($db, $query) or die("FOUT: " . mysqli_error());
 
+    ////// Neireau:
+    //
+    // Onderstaande functie heb ik in eerste instantie geschreven voor Events,
+    // uiteindelijk heb ik hem daar niet gebruikt maar gelukkig had ik hem wel
+    // bewaard.
+    // De while() loop maakt een associative array waarin de id voor profiel,
+    // en voornaam, foto en overmij om te tonen, worden opgeslagen.
+    //
+    //////
+
     while ($row = mysqli_fetch_assoc($result)) {
-      $geintereseerd  = $row['geintereseerd'];
-      $geslacht       = $row['geslacht'];
-    }
-
-    // Nu hebben we kandidaten nodig.
-    $query2  = "
-      SELECT profielen.gebruikers_id, gebruikers.voornaam, profielen.foto, profielen.overmij
-      FROM `gebruikers`
-      INNER JOIN profielen
-      ON gebruikers.gebruiker_id=profielen.gebruikers_id
-      WHERE gebruikers.geslacht='" . $geintereseerd . "'
-      AND profielen.geintereseerd='" . $geslacht . "'
-      ";
-    $result2 = mysqli_query($db, $query2) or die("FOUT: " . mysqli_error());
-
-    while ($row = mysqli_fetch_assoc($result2)) {
       $test[] = array(
           'id'            => $row['gebruikers_id'],
           'voornaam'      => $row['voornaam'],
@@ -57,10 +140,26 @@
       );
     }
 
-    // Genereer random shit.
+    ////// Neireau:
+    //
+    // Met de standaard functie rand_id() haal ik uiteindelijk twee geschikte,
+    // willekeurige kandidaten op die getoond gaan worden.
+    //
+    //////
+
     $rand_id = array_rand ($test, 2);
-    // echo $test[$rand_id[0]]['id'] . "<br>";
-    // echo $test[$rand_id[1]]['id'];
+
+    ////// Neireau:
+    //
+    // Onderstaande while() loop genereerd de daadwerkelijke content van de
+    // pagina. We hebben besloten twee profielen naast elkaar te tonen, buiten
+    // de loop wordt de container gegenereerd, daarbinnen wordt per $i een
+    // colom geschreven. $i Start bij nul omdat ik met een array() werk, deze
+    // starten altijd bij nul. Aan het einde van de colom wordt $i met één opgehoogd
+    // en de loop stopt wanneer $i één is, ofwel wanneer er twee colommen zijn.
+    //
+    //////
+
     $i = 0;
     echo "<div class=\"container\"><div class=\"row equal\">";
     while ($i < 2) {
@@ -94,7 +193,30 @@
 <head>
   <?php
 
+    ////// Neireau:
+    //
+    // 'header.php' Bevat een aantal functie die ik heb geschreven, deze functies
+    // worden op elke pagina gebruikt:
+    //
+    //////
+
     include "header.php";
+
+    ////// Neireau:
+    //
+    // De eerste functie, renderHead($titel), genereerd de <head> met daarin de
+    // verwijzingen naar Bootstrap en de <title> van de pagina, de titel kan wordt
+    // als variable mee gegeven.
+    // De daarop volgende functie, renderNavbar(), genereerd de navigatie balk die
+    // op elke pagina zichtbaar is, afhankelijk of je bent ingelogd en of je
+    // gebruiker danwel admin bent, worden er verschilende versies van de navbar
+    // getoond.
+    // De laatste functie, renderJumbotronMatchen(), is specifiek voor deze pagina
+    // geschreven. De code is grotendeels overgenomen van renderJumbotron(), een
+    // andere functie die ik heb geschreven die op andere pagina's aangeroepen
+    // kan worden, en bevat een extra knop om twee nieuwe matches te genereren.
+    //
+    //////
 
     renderHead('Date4Fun Matchen');
     renderNavbar();
@@ -109,15 +231,5 @@
       renderMatch();
 
     ?>
-    <script>
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close0")[0];
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-    </script>
   </body>
 </html>
---
+++ winkelwagen.bak.php
@@ -0,0 +1,254 @@
+<?php
+
+  error_reporting(E_ALL & ~E_NOTICE);
+
+  //include 'functions_gebruikersessie.php';
+  include "header.php";
+  include "config.php";
+
+  // moet niet ingelogd
+  if (1 ==1) {
+
+    session_start();
+
+    if ($_SESSION['gebruikers_id'] == NULL) {
+      header("Location:index.php");
+    }
+
+    if (empty($_SESSION['winkelwagen'])) {
+      $_SESSION['winkelwagen'] = array();
+    }
+
+?>
+
+
+<html>
+  <head>
+    <?php
+
+      renderHead('Date4Fun Winkelwagen');
+      renderNavbar();
+      // renderJumbotron();
+
+    ?>
+    <style>
+
+      .product-name {
+        margin-left: 20px;
+      }
+      .panel-body {
+        padding-top: 14px;
+      }
+
+    </style>
+    <?php
+
+      if(isset($_GET['del'])) {
+        verwijderUitWinkelWagen($_GET['del']);
+        winkelWagenOverzicht($db);
+      }
+
+      if(isset($_GET['add'])) {
+        toevoegenAanWinkelWagen($_GET['add']);
+        winkelWagenOverzicht($db);
+      }
+
+      if(isset($_GET['overzicht']))
+      {
+        winkelWagenOverzicht($db);
+      }
+
+
+      function toevoegenAanWinkelWagen($productid) {
+
+        if (in_array($productid, $_SESSION['winkelwagen'])) {
+          echo "<div class=\"container\">";
+          echo "<div class=\"row\">";
+          echo "<div class=\"alert alert-danger\"><strong>Product al aanwezig in winkelmand!</strong></div>";
+          echo "<div class=\"row\">";
+          echo "</div>";
+          echo "</div>";
+        }
+        else {
+          array_push($_SESSION[ 'winkelwagen'], $productid);
+        }
+      }
+
+      function verwijderUitWinkelWagen($productid) {
+
+        if (($product = array_search($productid, $_SESSION['winkelwagen'])) !== false) {
+          unset($_SESSION['winkelwagen'][$product]);
+        }
+        else
+        {
+          echo "Product komt niet voor in winkelwagen";
+          exit();
+        }
+      }
+
+      function winkelWagenOverzicht($db) {
+        // Hier staat niets voor zo ver ik kan zien.
+
+    ?>
+
+<div class="panel-body">
+
+    <?php
+
+      if (count($_SESSION['winkelwagen']) >= 1) {
+        $str =  implode(',', $_SESSION['winkelwagen']);
+
+        $sql = "SELECT IFNULL(events.titel, membership.omschrijving) as titel, producten.prijs, producten.product_id
+          FROM producten
+   	      LEFT JOIN events
+            ON events.event_id=producten.event_id
+          LEFT JOIN membership
+            ON membership.membership_id=producten.membership_id WHERE producten.product_id IN ($str)";
+
+        $result = mysqli_query($db, $sql);
+
+        $sql = "SELECT membership.korting as waarde
+          FROM membership
+          INNER JOIN members
+            ON members.membership_id=membership.membership_id
+              WHERE members.gebruiker_id = '".$_SESSION['gebruikers_id']."'";
+
+        $results = mysqli_query($db, $sql);
+
+        $korting = mysqli_fetch_assoc($results);
+
+    ?>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-8 col-md-offset-2">
+          <div class="panel panel-info">
+            <div class="panel-heading">
+              <div class="panel-title">
+                <div class="row">
+                  <div class="col-xs-6">
+                    <h4><span class="glyphicon glyphicon-shopping-cart"></span> Winkelwagen inhoud</h4>
+                  </div>
+                  <div class="col-xs-6">
+                     <a href="./events.php">
+                       <button type="button" class="btn btn-default btn-lg btn-block">
+                         <span class="glyphicon glyphicon-share-alt"></span>
+                         Verder winkelen
+                       </button>
+                     </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+    <?php
+
+        while($product = mysqli_fetch_assoc($result)) {
+
+    ?>
+
+
+                  <div class="row">
+
+                    <div class="col-xs-6">
+
+                      <h4 class="product-name"><strong><?php echo $product['titel']; ?> </strong></h4><h4><small></small></h4>
+
+                    </div>
+                    <div class="col-xs-6">
+                      <div class="col-xs-6 text-right">
+                        <h6><strong><?php echo $product['prijs']; ?> <span class="text-muted"></span></strong></h6>
+                      </div>
+                      <div class="col-xs-4">
+                      </div>
+                      <div class="col-xs-2">
+                        <a href="./winkelwagen.php?del=<?php echo $product['product_id']; ?>" <button type="button" class="btn btn-link btn-md">
+
+
+                          <span class="glyphicon glyphicon-trash"> </span>
+                        </button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+
+                <hr>
+
+
+
+
+
+            </div>
+            <div class="panel-footer">
+              <div class="row text-center">
+                <div class="col-xs-9">
+                  <h4 class="text-right">Totaal <strong>50.00</strong></h4>
+                </div>
+                <div class="col-xs-3">
+                  <form action="betaald.php" method="post">
+                    <button type="submit" name="afrekenen" value="Afrekenen" class="btn btn-success btn-block">
+                      Afrekenen
+                    </button>
+                    <input type="hidden" name="product_id" value="<?php echo $product['product_id']?>">
+                    <input type="hidden" name="gebruiker_id" value="<?php echo $_SESSION['gebruikers_id']?>">
+                  </form>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <?php
+      }
+    }
+    else {
+      ?>
+
+
+
+
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-8 col-md-offset-2">
+            <div class="panel panel-info">
+              <div class="panel-heading">
+                <div class="panel-title">
+                  <div class="row">
+                    <div class="col-xs-6">
+                      <h4><span class="glyphicon glyphicon-shopping-cart"></span> Winkelwagen inhoud</h4>
+                    </div>
+                    <div class="col-xs-6">
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="panel-body">
+                <div class="row">
+
+                  <div class="text-center">
+                    <h4>U heeft momenteen geen producten in uw winkelwagen</h4>
+                  </div>
+
+                </div>
+              </div>
+              <div class="panel-footer">
+                <div class="row text-center">
+                  <div class="col-xs-9">
+                  </div>
+                  <div class="col-xs-3">
+                    <a href="./events.php"><button type="button" class="btn btn-default btn-lg btn-block">
+                      <span class="glyphicon glyphicon-share-alt"></span> winkelen
+                    </button><a/>
+                  </div>
+                </div>
+              </div>
+
+
+            <?php
+
+          }}}
+
+            ?>
